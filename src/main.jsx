74142d538d49e73d7b66c2e8571a78be91f76337
Nicolas Vargas <nicolasvargas846@gmail.com>
2023-05-25 16:13:43 -0500

    primer commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './assets/styles/styles.css'
+import { HomePage } from './Pages/HomePage.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HomePage />
+  </React.StrictMode>,
+)
