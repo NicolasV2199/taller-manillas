4d7a1ede40547e6d4a6f9510458360f1b7d7cd48
Nicolas Vargas <nicolasvargas846@gmail.com>
2023-05-26 19:24:26 -0500

    realizando ajustes y agregando selects

--- src/Components/OptionCard.jsx
+++ src/Components/OptionCard.jsx
@@ -8,9 +8,9 @@ export const OptionCard = ({ option }) => {
 
   return (
     <>
-      <div className="card m-3 p-3">
+      <div className="card material-card my-2 mx-3">
         <h3 className="text-center">{option.title}</h3>
-        <img src={option.image} alt="" />
+        <img src={option.image} alt="" className="my-4 rounded" />
         <select className="form-select" aria-label="Default select example" value={selectValue} onChange={(e) => setSelectValue(e.target.value)}>
           {
             option.list.map((item) => (
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,69 +1,142 @@
 // import { useState } from "react"
+import { NavBar } from "../Components/NavBar"
 import { OptionsList } from "../Components/OptionsList"
 
 const options = [
   {
     title: "Material",
-    image: 'https://i.pinimg.com/originals/06/83/2f/06832f9477c1a91538e9a5892530fd71.jpg',
-    list: [{
+    image: '/assets/images/materiales.jpeg',
+    list: [
+      {
         id: 1,
-        name: 'Cuero',
-        value: 'Cuero',
+        name: 'Leather',
+        value: 'Leather',
       },
       {
         id: 2,
-        name: 'Cuerda',
-        value: 'Cuerda',
+        name: 'Rope',
+        value: 'Rope',
       },
     ]
   },
   {
-    title: "Dije",
-    image: 'https://i.pinimg.com/originals/06/83/2f/06832f9477c1a91538e9a5892530fd71.jpg',
-    list: [{
+    title: "Charm",
+    image: '/assets/images/dijes.jpeg',
+    list: [
+      {
         id: 3,
-        name: 'Martillo',
-        value: 'Martillo',
+        name: 'Hammer',
+        value: 'Hammer',
       },
       {
         id: 4,
-        name: 'Ancla',
-        value: 'Ancla',
+        name: 'Anchor',
+        value: 'Anchor',
       },
     ]
   },
   {
-    title: "Tipo",
-    image: 'https://i.pinimg.com/originals/06/83/2f/06832f9477c1a91538e9a5892530fd71.jpg',
-    list: [{
+    title: "Type",
+    image: '/assets/images/tipos.jpeg',
+    list: [
+      {
         id: 5,
-        name: 'Oro, Oro Rosado',
-        value: 'Oro',
+        name: 'Gold',
+        value: 'Gold',
       },
       {
         id: 6,
-        name: 'Plata',
-        value: 'Plata',
+        name: 'Pink Gold',
+        value: 'Gold',
       },
       {
         id: 7,
-        name: 'Niquel',
-        value: 'Niquel',
+        name: 'Silver',
+        value: 'Silver',
+      },
+      {
+        id: 8,
+        name: 'Nickel',
+        value: 'Nickel',
       },
     ]
   },
- 
+
 ]
 
 export const HomePage = () => {
   return (
     <>
+      <NavBar />
       <div className="home-container">
-        <h1 className="text-center">Venta de Manillas Personalizadas</h1>
-        <p>Nuestras manillas de alta calidad pueden ser fabricadas con distintos materiales, por favor escoja a continuación los materiales que desea para sus manillas: </p>
+        <div className="card main-card">
+          <h1 className="text-center">Sale of personalized bracelets</h1>
+          <p className="text-center">Our high quality bracelets can be made with different materials, please choose the materials you want for your bracelets below: </p>
 
-        <OptionsList options={options} />
+          <OptionsList options={options} />
 
+          <div className="row mt-3">
+
+            <div className="form-group col-md-4">
+              <label htmlFor="name">Buyers name</label>
+              <div className="input-group">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Enter your name"
+                  id="name"
+                />
+              </div>
+            </div>
+
+            <div className="form-group col-md-4">
+              <label htmlFor="quantity">Bracelets quantity</label>
+              <div className="input-group">
+                <input
+                  type="number"
+                  className="form-control"
+                  id="quantity"
+                  placeholder="Enter the number of bracelets you want"
+                />
+              </div>
+            </div>
+
+            <div className="form-group col-md-4">
+              <label htmlFor="quantity">Bracelets quantity</label>
+              <div className="input-group">
+                <input
+                  type="number"
+                  className="form-control"
+                  id="quantity"
+                  placeholder="Enter the number of bracelets you want"
+                />
+                <div className="input-group-append">
+                  <button className="btn btn-primary">
+                    Confirm
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            {/* <div className="form-group col-md-4">
+              <label htmlFor="currency">Currency</label>
+              <div className="input-group">
+                <select className="form-select" id="currency">
+                  <option value="Dolars">Dolars</option>
+                  <option value="Pesos">Colombian pesos</option>
+                </select>
+              </div>
+            </div> */}
+
+          </div>
+
+          <div className="row mt-5">
+            <div className="col-md-12">
+              <h2 className="uppercase text-center">Final price: Dolares: $5000 Pesos Colombianos: $25.000.000</h2>
+            </div>
+          </div>
+
+        </div>
       </div>
     </>
   )
